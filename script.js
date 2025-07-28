8ad9f47f8bf51cbb5033ca2fd0dd7a8baee5afaf
matthewvadeboncoeur <151592935+matthewvadeboncoeur@users.noreply.github.com>
2025-07-28 15:26:28 -0700

    added ability to remove tasks and added animations

--- script.js
+++ script.js
@@ -1,10 +1,49 @@
 const button = document.getElementById('long-button');
-button.addEventListener('click', func1);
-function func1() {
+
+
+button.addEventListener('click', add_task);
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'Enter')
+        add_task();
+});
+
+
+function add_task() {
     var li = document.createElement("li");
+    var spanLi = document.createElement("SPAN");
     var input_value = document.getElementById("long-input").value;
+    if (input_value == "")
+        return;
     var t = document.createTextNode(input_value);
-    li.appendChild(t);
+    spanLi.appendChild(t);
+    li.appendChild(spanLi);
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = 'close';
+    span.appendChild(txt);
+    li.appendChild(span);
     document.getElementById('all-tasks').appendChild(li);
     document.getElementById('long-input').value='';
+    add_close();
+}
+
+function add_close() {
+    var all_close_buttons = document.getElementsByClassName('close');
+    for (var i = 0; i < all_close_buttons.length; i++) {
+        all_close_buttons[i].addEventListener('click', remove_task);
+        all_close_buttons[i].addEventListener('mouseover', make_red);
+        all_close_buttons[i].addEventListener('mouseleave', make_black);
+    }
+}
+
+function remove_task() {
+    this.parentElement.remove();
+}
+
+function make_red() {
+    this.parentElement.querySelector("SPAN").style.color = 'red';
+}
+
+function make_black() {
+    this.parentElement.querySelector("SPAN").style.color = 'black';
 }
