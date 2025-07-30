6b59838507077e363994a0830cf3e692763e0de1
matthewvadeboncoeur <151592935+matthewvadeboncoeur@users.noreply.github.com>
2025-07-29 22:07:19 -0700

    did a lot of stuff lol

---
+++ backend/server.js
@@ -0,0 +1,107 @@
+const express = require('express')
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+mongoose.connect('mongodb+srv://vadebonm:IUQb55CKhWI3uIqK@cluster0.ovuczhl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    .then(() => console.log('✅ Connected to MongoDB Atlas'))
+    .catch(err => console.error('❌ MongoDB connection error:', err));
+
+const taskSchema = new mongoose.Schema({
+    text: {type: String, required: true},
+    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true}
+})
+
+const userSchema = new mongoose.Schema({
+    username: {type: String, required: true, unique: true},
+    password: {type: String, required: true}
+})
+
+const User = mongoose.model('User', userSchema)
+const Task = mongoose.model('Task', taskSchema)
+
+const cors = require('cors')
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+
+app.post('/signup', async (req, res) => {
+    try {
+        const {username, password} = req.body;
+        const existingUser = await User.findOne({username})
+        if (existingUser) {
+            return res.status(400).json({message: 'Username already taken'})
+        }
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const newUser = new User({username, password: hashedPassword})
+        await newUser.save()
+        res.status(201).json({message: 'User created successfully!'})
+
+    }   catch (err) {
+        res.status(500).json({message: 'Error creating user', error: err})
+    }
+
+})
+
+app.post('/login', async (req, res) => {
+    try {
+        const {username, password} = req.body;
+        const user = await User.findOne({username})
+        if (!user) {
+            return res.status(400).json({message: 'Invalid username or password'})
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({message: 'Invalid username or password'})
+        }
+        const token = jwt.sign({userId: user._id}, 'SECRET_KEY')
+        res.json({message: 'Login successful!', token})
+
+    } catch (err) {
+        res.status(500).json({message: 'Error logging in', error: err})
+    }
+})
+
+
+function authMiddleware(req, res, next) {
+    const authHeader = req.headers.authorization
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({message: 'No token provided'})
+    }
+    const token = authHeader.split(' ')[1]
+    try {
+        const decoded = jwt.verify(token, 'SECRET_KEY')
+        req.userId = decoded.userId
+        next()
+    } catch (err) {
+        return res.status(401).json({message: 'Invalid or expired token'})
+    }
+}
+
+
+
+app.post('/tasks', authMiddleware, async (req, res) => {
+    const task = new Task({ text: req.body.text, userId: req.userId })
+    await task.save();
+    res.status(201).json({ message: 'Task saved!', task})
+})
+
+
+app.get('/tasks', authMiddleware, async (req, res) => {
+    const tasks = await Task.find({userId: req.userId})
+    res.json(tasks)
+})
+
+app.delete('/tasks/:id', authMiddleware, async (req, res) => {
+    const task = await Task.findOneAndDelete({_id: req.params.id, userId: req.userId})
+    if (!task) {
+        return res.status(404).json({message: 'Task not found or not yours'})   
+    }
+
+    res.json({message: 'Task deleted', task})
+})
+
+
+app.listen(3000, () => {
+    console.log(`Server is running on http://localhost:3000}`);
+});
--- script.js
+++ script.js
@@ -1,30 +1,104 @@
+load_tasks()
 const button = document.getElementById('long-button');
-
-
 button.addEventListener('click', add_task);
 document.addEventListener('keydown', function(e) {
     if (e.key === 'Enter')
         add_task();
 });
 
+const signup_button = document.getElementById('signup_button')
+const login_button = document.getElementById('login_button')
+
+signup_button.addEventListener('click', userSignup)
+login_button.addEventListener('click', userLogin)
+
+
+function userSignup() {
+    const username = document.getElementById('username_input').value
+    const password = document.getElementById('password_input').value
+
+    if (username === '' || password === '')
+        return;
+
+    fetch('http://localhost:3000/signup', {
+        method: 'POST', headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({'username': username, 'password': password})
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert(data.message)
+    })
+}
+
+function userLogin() {
+    const username = document.getElementById('username_input').value
+    const password = document.getElementById('password_input').value
+
+    if (username === '' || password === '')
+        return;
+
+    fetch('http://localhost:3000/login', {
+        method: 'POST', headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({'username': username, 'password': password})
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert(data.message)
+        localStorage.setItem('token', data.token)
+    })
+}
+
+
+function load_tasks() {
+    fetch('http://localhost:3000/tasks')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(task => {
+            const li = document.createElement("li");
+            li.setAttribute('data-id', task._id);
+            const spanLi = document.createElement("SPAN");
+            const t = document.createTextNode(task.text);
+            spanLi.appendChild(t);
+            li.appendChild(spanLi);
+
+            const span = document.createElement("SPAN");
+            const txt = document.createTextNode("\u00D7");
+            span.className = 'close';
+            span.appendChild(txt);
+            li.appendChild(span);
+            document.getElementById('all-tasks').appendChild(li);
+        })
+        add_close();
+    })
+}
+
 
 function add_task() {
-    var li = document.createElement("li");
     var spanLi = document.createElement("SPAN");
     var input_value = document.getElementById("long-input").value;
-    if (input_value == "")
-        return;
-    var t = document.createTextNode(input_value);
-    spanLi.appendChild(t);
-    li.appendChild(spanLi);
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = 'close';
-    span.appendChild(txt);
-    li.appendChild(span);
-    document.getElementById('all-tasks').appendChild(li);
-    document.getElementById('long-input').value='';
-    add_close();
+    if (input_value == "") return;
+
+    fetch('http://localhost:3000/tasks', {
+        method: 'POST', headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({text: input_value})
+    })
+    .then(response => response.json())
+    .then(data => {
+        var li = document.createElement("li");
+        li.setAttribute('data-id', data.task._id);
+        console.log('Server response:', data);
+        var t = document.createTextNode(input_value);
+        spanLi.appendChild(t);
+        li.appendChild(spanLi);
+        var span = document.createElement("SPAN");
+        var txt = document.createTextNode("\u00D7");
+        span.className = 'close';
+        span.appendChild(txt);
+        li.appendChild(span);
+        document.getElementById('all-tasks').appendChild(li);
+        document.getElementById('long-input').value='';
+        add_close();
+    })
 }
 
 function add_close() {
@@ -37,7 +111,18 @@ function add_close() {
 }
 
 function remove_task() {
-    this.parentElement.remove();
+    const li = this.parentElement;
+    const taskId = li.getAttribute('data-id')
+    fetch(`http://localhost:3000/tasks/${taskId}` , {
+        method: 'DELETE'
+    })
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        console.log('Deleted from backend:', data);
+        li.remove();
+    })
 }
 
 function make_red() {
@@ -47,3 +132,4 @@ function make_red() {
 function make_black() {
     this.parentElement.querySelector("SPAN").style.color = 'black';
 }
+
